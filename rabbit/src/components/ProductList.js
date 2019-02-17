f56f3d53e9cf03bb2fb8b52097be4c2288f7d1d8
diegoajv <diegojosevilla@outlook.com>
2019-02-17 01:43:25 -0600

    Fix[product-list]: Add props to product-list

--- rabbit/src/components/ProductList.js
+++ rabbit/src/components/ProductList.js
@@ -3,9 +3,22 @@ import Product from './Product';
 
 class ProductList extends Component {
     render() {
+        const products = this.props.products.map((product) => (
+            <Product
+                key={'product-' + product.id}
+                id={product.id}
+                title={product.title}
+                description={product.description}
+                url={product.url}
+                votes={product.votes}
+                submitterAvatarUrl={product.submitterAvatarUrl}
+                productImageUrl={product.productImageUrl}
+            />
+        ));
+
         return (
             <div className='ui unstackable items'>
-                <Product />
+                {products}
             </div>
         )
     }

2104cd9bcd8e9c9de84302ead30ac85d1e981c04
diegoajv <diegojosevilla@outlook.com>
2019-02-17 01:44:38 -0600

    Fix[app]: Add seed data to app component

--- rabbit/src/App.js
+++ rabbit/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import data from './seeds/data';
 import ProductList from './components/ProductList';
 
 class App extends Component {
   render() {
     return (
-      <ProductList />
+      <ProductList products={data.products}/>
     );
   }
 }

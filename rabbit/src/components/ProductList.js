dfdc13f7482a470bfbc6cea63119471f983d982d
diegoajv <diegojosevilla@outlook.com>
2019-03-15 06:31:34 -0600

    Fix[product-list]: Move products from render to component state

--- rabbit/src/components/ProductList.js
+++ rabbit/src/components/ProductList.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 import Product from './Product';
 
 class ProductList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            products: []
+        };
+    }
+
+    componentDidMount() {
+        this.setState({products: this.props.products});
+    }
+
     handleProductUpVote(productId) {
         console.log(productId + ' was upvoted');
     }
 
     render() {
-        const products = this.props.products.sort((a, b) => (b.votes - a.votes));
+        const products = this.state.products.sort((a, b) => (b.votes - a.votes));
 
         const productComponents = products.map((product) => (
             <Product
